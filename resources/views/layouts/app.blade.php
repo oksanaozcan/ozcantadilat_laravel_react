97bde2821d5530de41045105b468e077eb475260
oksanaozcan <mekansa83@gmail.com>
2022-04-07 12:13:05 +0300

    added google translate API

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,19 @@
   <title>Ozcan Tadilat</title>
 </head>
 <body>
-  <div id="root"></div>    
+  <div id="root"></div>  
+  
+  <script>
+    function googleTranslateElementInit() {
+      const config={
+        pageLanguage: 'tr',
+        includedLanguages: 'en,ru',
+        layout:google.translate.TranslateElement.InlineLayout.SIMPLE
+      }
+      const langOptionsID='google_translate_element';
+      new google.translate.TranslateElement(config,langOptionsID);
+    }
+  </script>
+  <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </body>
 </html>
